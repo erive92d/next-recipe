538c98935e48344de9f3cb33441d4bb0155d52b7
Deorren Erive <mac@Deorrens-MacBook-Air.local>
2023-12-11 20:32:38 -0800

    s

--- components/mainComps/PeopleChoice.tsx
+++ components/mainComps/PeopleChoice.tsx
@@ -10,14 +10,16 @@ type PeopleChoiceType = {
 
 export default async function PeopleChoice() {
 
-    const saves = await fetch("http://localhost:3000/api/recipe", {
+    const response = await fetch(`http://localhost:3000/api/recipe`, {
+        method: "GET",
         cache: "no-store"
     })
-    const response: PeopleChoiceType[] = await saves.json()
+
+    const save: PeopleChoiceType[] = await response.json()
 
     return (
         <div className="p-2 bg-white">
-            {response.map((res) => (
+            {save.map((res) => (
                 <div key={res.id} className="flex justify-between">
                     <h1>{res.name}</h1>
                     <p>{res.users.length} saves</p>
@@ -25,5 +27,6 @@ export default async function PeopleChoice() {
             ))}
         </div>
     )
+
 }
 
