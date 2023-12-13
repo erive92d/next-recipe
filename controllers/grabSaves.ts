aca2d42535f2d8a27973f7646d730f3fe99dab63
Deorren Erive <mac@Deorrens-MacBook-Air.local>
2023-12-12 20:47:42 -0800

    9

--- controllers/grabSaves.ts
+++ controllers/grabSaves.ts
@@ -1,7 +1,5 @@
-import serverUrl from "@/lib/serverUrl"
 
 export default async function grabSaves() {
-    console.log(process.env.NEXT_PUBLIC_API_URL)
     try {
         const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/recipeapi/recipesaves`, {
             method: "GET",
